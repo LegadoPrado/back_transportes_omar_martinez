9bb5885d5904ae2cd9f60257a877cd19cb55bb48
Efren Prado <emmanuel941308@gmail.com>
2021-10-08 11:28:13 -0500

    modificacion de variables de entorno e instalacion y configuracion de nodemon

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   port: process.env.PORT || 3000,
 
-  dbTaxiUser: process.env.DB_TAXI_USER || "",
-  dbTaxiPassword: process.env.DB_TAXI_PASSWORD || "",
-  dbTaxiServer: process.env.DB_TAXI_SERVER || "",
-  dbTaxiDatabase: process.env.DB_TAXI_DATABASE || "",
+  dbSqlTransportesOMUser: process.env.DB_SQL_TRANSPORTES_OM_USER || "",
+  dbSqlTransportesOMPassword: process.env.DB_SQL_TRANSPORTES_OM_PASSWORD || "",
+  dbSqlTransportesOMServer: process.env.DB_SQL_TRANSPORTES_OM_SERVER || "",
+  dbSqlTransportesOMDatabase: process.env.DB_SQL_TRANSPORTES_OM_DATABASE || "",
 };
